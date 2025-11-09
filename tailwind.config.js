0a8c5ae806efb1ffce2b22d68c8ce70cb1007f4f
Yasamin-E84 <yasaminsoraghi84@gmail.com>
2025-11-09 18:40:19 +0330

    finished user-account

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,3 +4,12 @@ export default {
   theme: { extend: {} },
   plugins: [],
 }
+// tailwind.config.js
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  safelist: [
+    'bg-[#08237D]',
+    'border-[#08237D]',
+    'text-[#FFB800]',
+  ],
+}
